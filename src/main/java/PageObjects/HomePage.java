f350614ff8cb0b951463a19cd76ca64360dc187d
mohit <mohitkwatra519@gmail.com>
2025-10-05 19:13:15 +0530

    Commit with exceptions

--- src/main/java/PageObjects/HomePage.java
+++ src/main/java/PageObjects/HomePage.java
@@ -1,13 +1,19 @@
 package PageObjects;
 
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import blisspadsAutomation.AbstractComponents.commonUtility;
+import exceptions.FrameworkExceptions;
+import exceptions.FrameworkExceptions.FrameworkException;
+import utils.LoggerUtil;
 
 public class HomePage extends commonUtility {
+	private static final Logger logger = LoggerUtil.getLogger(HomePage.class) ;
+	
 	WebDriver driver ; 
 	
 	public HomePage(WebDriver driver) {
@@ -20,13 +26,26 @@ public class HomePage extends commonUtility {
 	WebElement napkins;
 	
 	public ProductPage clickSanitaryNapkins() {
-		napkins.click();
-		ProductPage product = new ProductPage(driver);
-		return product ;
+		try {
+			logger.info("clicking on napkins button");
+			napkins.click();
+			ProductPage product = new ProductPage(driver);
+			return product ;
+		}
+		
+		catch (Exception e) {
+            logger.error("Checkout button click failed", e);
+            throw new FrameworkException("Checkout button click failed", e);
+        }
+		
+		
 	}
 	
 	public void goTo() {
 		driver.get("https://www.blissnatural.in");
+		
+		
+		
 	}
 	
 
--- src/main/java/PageObjects/ReviewPage.java
+++ src/main/java/PageObjects/ReviewPage.java
@@ -1,5 +1,6 @@
 package PageObjects;
 
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,8 +8,12 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import blisspadsAutomation.AbstractComponents.commonUtility;
+import exceptions.FrameworkExceptions;
+import exceptions.FrameworkExceptions.ElementNotFoundException;
+import utils.LoggerUtil;
 
 public class ReviewPage extends commonUtility{
+	private static final Logger logger = LoggerUtil.getLogger(ReviewPage.class);
 	WebDriver driver ; 
 	
 	public ReviewPage(WebDriver driver) {
@@ -29,15 +34,27 @@ public class ReviewPage extends commonUtility{
 	
 	
 	public void selectPack() {
-		pack.click();
+		try	
+		{
+			logger.info("User is Clicking on pack button");
+			pack.click();
+		}
+		catch (Exception e) {
+			logger.error("User is unable to click pack button");
+			throw new ElementNotFoundException("Pack button not found", e);
+		}
 	}
 	
 	public void Quantity() {
 		increaseQty.click();
 	}
 	public cartPage clickAddtoCart() {
-		addToCart.click();
-		return new cartPage(driver);
+	    try {
+	        addToCart.click();
+	    } catch (Exception e) {
+	        throw new ElementNotFoundException("Failed to click 'Add to Cart'. Element might be missing, not clickable, or blocked.", e);
+	    }
+	    return new cartPage(driver);
 	}
 	
 	
